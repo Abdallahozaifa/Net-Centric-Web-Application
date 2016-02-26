b461001f3adeea2599bfd4e787bc4aef2d5f9dee
alkief <alk5572@psu.edu>
2016-02-26 16:24:46 -0500

    Added files via upload

---
+++ Website/EvalJSONP/quiz-questions.js
@@ -0,0 +1,78 @@
+{"Questions":[
+    {
+        "Question": "1. Which of the following is not a valid CSS selector?",
+        "A": "div, p",
+        "B": "[target=_blank]",
+        "C": "#p, [target]",
+        "D": "a:active"
+    },
+    {
+        "Question": "2. Where can bubbling be stopped from?",
+        "A": "Bubbling cannot be stopped",
+        "B": "Anywhere on the propogation tree",
+        "C": "Only from the object on which the event was registered",
+        "D": "Only through the containing window object"
+
+    },
+    {
+        "Question": "3. Which of the following events is an example of an exception to element bubbling?",
+        "A": "Scroll Events",
+        "B": "Button Clicks",
+        "C": "Page Loading",
+        "D": "Mouse Drag"
+
+    },
+    {
+        "Question": "4. The three phases involved in event propogation are?",
+        "A": "Capturing, Propogation, Bubbling",
+        "B": "Propogation, Invocation, Bubbling",
+        "C": "Capturing, Invocation, Bubbling",
+        "D": "Bubbling, Invocation, Execution"
+
+    },
+    {
+        "Question": "5. Which is not a valid state for an anchor object?",
+        "A": "Clicked",
+        "B": "Visited",
+        "C": "Link",
+        "D": "Hover"
+    },
+    {
+        "Question": "6. The setTimeout() and setInterval() functions belong to which JavaScript object?",
+        "A": "Document",
+        "B": "Window",
+        "C": "They are local to each browser page",
+        "D": "They are predefined as event objects"
+
+    },
+    {
+        "Question": "7. JavaScipt files cannot be",
+        "A": "Included on an HTML page as a local file",
+        "B": "Included on an HTML page as an external file",
+        "C": "Written in-line on an HTML page",
+        "D": "Run as a standalone file"
+    },
+    {
+        "Question": "8. What does CSS stand for?",
+        "A": "Creative Style Sheets",
+        "B": "Colorful Style Sheets",
+        "C": "Computer Style Sheets",
+        "D": "Cascading Style Sheets",
+
+    },
+    {
+        "Question": "9. Client-side scripts have access to cookies through what object?",
+        "A": "Window",
+        "B": "Document",
+        "C": "localStorage",
+        "D": "Browser"
+
+    },
+    {
+        "Question": "10. Which of the following is not a JavaScript keyword?",
+        "A": "this",
+        "B": "return",
+        "C": "screen",
+        "D": "button"
+    }    
+]};
